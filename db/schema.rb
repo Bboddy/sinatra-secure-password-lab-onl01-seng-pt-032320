2fecb43c5f16b81347b30d19f781336e96cd0782
Bryce B <bryce.boddy@gmail.com>
2020-07-17 01:00:50 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200717005413) do
+ActiveRecord::Schema.define(version: 20200717010027) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
