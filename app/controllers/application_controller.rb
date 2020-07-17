04451ade42c04f518c0328472dc47f54be68dd96
Bryce B <bryce.boddy@gmail.com>
2020-07-17 00:38:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,8 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     @user = User.find_by(username: params[:username])
     if params[:username].empty? || params[:password].empty?
-    if @user.authenticate(params[:password])
+      redirect '/failure'
+    elsif @user.authenticate(params[:password])
       session[user_id] = @user.id
       redirect to "/account"
     else
